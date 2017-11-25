67cd9724a547aaaf59a7e57fa09b26a10e8f384a
Pedro Tommaselli <pedrotommaselli@gmail.com>
2017-11-25 17:29:43 -0300

    fd

---
+++ js/verperir.js
@@ -0,0 +1,94 @@
+class vermaior{
+	constructor(title,foto,legenda){
+		this.title = title;
+		this.foto = foto;
+		this.legenda = legenda;
+	}
+
+	getTitle(){
+		return this.title;
+
+	}
+	getFoto(){
+		return this.foto;
+	}
+	getLegenda(){
+		return this.legenda;
+	}
+}
+
+
+var cup1 = new vermaior("CupCake Flores","ck/cup1.jpg","Massa de Baunilha, Cobertura de Morango, Recheio de Creme e Enfeites de Flores");
+var cup2 = new vermaior("CupCake ChocoMenta","ck/cup2.jpg","Massa de Chocolate, Cobertura de Menta e Recheio de Chocolate");
+var cup3 = new vermaior("CupCake RedCream","ck/cup3.jpg","Massa de Baunilha, Cobertura de Morango e Creme e Recheio de Creme");
+var cup4 = new vermaior("CupCake BlueLove","ck/cup4.jpg","Massa de Chocolate, Cobertura de Chantilly, Recheio de Chocolate e Enfeites de Coração");
+var cup5 = new vermaior("CupCake GreenLove","ck/cup5.jpg","Massa de Chocolate, Cobertura de Creme cor Verde e Recheio de Chocolate");
+var cup6 = new vermaior("CupCake RomanticCream","ck/cup6.jpg","Massa de Chocolate, Cobertura de Creme, Recheio de Chocolate e Enfeites de Coração");
+var cup7 = new vermaior("CupCake Red Fruits","ck/cup7.jpg","Massa de Chocolate, Cobertura de Creme (Frutas Vermelhas), Recheio de Chocolate e Enfeite de Framboesa");
+var cup8 = new vermaior("CupCake ChocoGreen","ck/cup8.jpg","Massa de Chocolate, Cobertura de Menta e Recheio de Chocolate");
+var cup9 = new vermaior("CupCake Rose","ck/cup9.jpg","Massa de Chocolate, Cobertura de Creme (morango) em Forma de uma Flor e Recheio de Chocolate");
+var cup10 = new vermaior("CupCake IceCream","ck/cup10.jpg","Massa de Baunilha, Cobertura de Creme e Chocolate, Recheio de Creme de Morango e Enfeite de Cereja");
+var cup11 = new vermaior("CupCake RosePink","ck/cup11.jpg","Massa de Chocolate, Cobertura de Chantilly (morango), Recheio de Morango e Enfeite de uma Flor");
+var cup12 = new vermaior("CupCake BananaLove","ck/cup12.jpg","Massa de Banana, Cobertura de Creme (banana), Recheio de Creme e Enfeite de Coração");
+var cup13 = new vermaior("CupCake Strawberry","ck/cup13.jpg","Massa de Baunilha, Cobertura de Creme (morango), Recheio de Morango e Enfeite de bolinhas prateadas");
+var cup14 = new vermaior("CupCake PurpleCream","ck/cup14.jpg",);
+var cup15 = new vermaior("CupCake Unicorn","ck/cup15.jpg","Massa de Baunilha, Cobertura de Caramelo e Creme (morango), Recheio de Morango e Enfeite de boneco de Unicórnio de Açucar");
+var cup16 = new vermaior("CupCake ChocoDrop","ck/cup16.jpg","Massa de Chocolate, Cobertura de Chocolate, Recheio de Chocolate e Enfeite de Gotas de Chocolate");
+var cup17 = new vermaior("CupCake Neapolitan","ck/cup17.jpg","Massa de Baunilha, Cobertura de Creme (chocolate, morango e baunilha), Recheio de Creme e Enfeite de Bolacha e Cereja");
+var cup18 = new vermaior("CupCake Chestnut","ck/cup18.jpg","Massa de Baunilha, Cobertura de Caramelo, Recheio de Caramelo e Enfeite de Castanha");
+var cup19 = new vermaior("CupCake Peanuts","ck/cup19.jpg","Massa de Chocolate, Cobertura de Creme e Amendoas e Recheio de Chocolate");
+var cup20 = new vermaior("CupCake Cream","ck/cup20.jpg","Massa de Chocolate, Cobertura de Creme, Recheio de Morango e Enfeite de Morango");
+var cup21 = new vermaior("CupCake Coffe","ck/cup21.jpg","Massa de Chocolate com Café, Cobertura de Chantilly, Recheio de Café e Enfeite de Pó de Café Doce");
+var cup22 = new vermaior("CupCake Chocolate","ck/cup22.jpg","Massa de Chocolate Forte, Cobertura de Musse de Chocolate, Recheio de Chocolate e Granulado");
+var cup23 = new vermaior("CupCake CherryCream","ck/cup23.jpg","Massa de Baunilha, Cobertura de Chantilly, Recheio de Creme e Enfeite de Granulado e Cereja");
+var cup24 = new vermaior("CupCake ChocoLove","ck/cup24.jpg","Massa de Chocolate Forte, Cobertura de Musse de Chocolate, Recheio de Chocolate e Enfeite de Coração e Granulado");
+var cup25 = new vermaior("CupCake ChocoBalls","ck/cup25.jpg","Massa de Chocolate, Cobertura de Chocolate, Recheio de Chocolate e Enfeite de Bolas de Chocolate");
+var cup26 = new vermaior("CupCake VanillaCream","ck/cup26.jpg","Massa de Baunilha, Cobertura de Creme, Recheio de Baunilha e Enfeite de Coração");
+var cup27 = new vermaior("CupCake Caramel","ck/cup27.jpg","Massa de Caramelo, Cobertura de Caramelo e Recheio de Caramelo");
+var cup28 = new vermaior("CupCake Açai ","ck/cup28.jpg","Massa de Chocolate, Cobertura de Açai, Recheio de Açai e Enfeite de Granulado");
+var cup29 = new vermaior("CupCake ChocoChantilly","ck/cup29.jpg","Massa de Chocolate, Cobertura de Chantilly, Recheio de Chocolate e Enfeite de Calda de Chocolate");
+var cup30 = new vermaior("CupCake Papaya","ck/cup30.jpg","Massa de, Cobertura de, Recheio de e");
+var cup31 = new vermaior("CupCake BananaChoco","ck/cup31.jpg","Massa de Banana, Cobertura de Banana e Caramelo e Recheio de Chocolate");
+var cup32 = new vermaior("CupCake BananaCream","ck/cup32.jpg","Massa de Banana, Cobertura de Creme com Chocolate, Recheio de Banana");
+var cup33 = new vermaior("CupCake LoveRose","ck/cup33.jpg","Massa de Baunilha, Cobertura de Creme, Recheio de Creme e Endeite de Flor");
+
+
+var sla = [];
+
+sla.push(cup1, cup2, cup3, cup4, cup5, cup6, cup7, cup8, cup9,
+		cup10, cup11, cup12, cup13, cup14, cup15, cup16, cup17,
+		cup18, cup19, cup20, cup21, cup22, cup23, cup24, cup25,
+		cup26, cup27, cup28, cup29, cup30, cup31, cup32, cup33);
+
+
+for(var i = 0; i < sla.length; i++){
+
+	var divs = document.createElement("div");
+	divs.class = "cards";
+
+	var as = document.createElement("a");
+	as.id = "clickimg";
+	
+	var fots = document.createElement("img");
+	fots.id = "foto";
+	fots.src = sla[i].foto;
+
+	var hs = document.createElement("h3");
+	hs.id = "cknome"
+	hs.innerHTML = sla[i].title;
+
+	var leg = document.createElement("legend");
+	leg.id = "desc";
+	leg.innerHTML = sla[i].legenda;
+
+	as.appendChild(fots);
+	divs.appendChild(as);
+	divs.appendChild(hs);
+	divs.appendChild(leg);
+
+	var recebe = document.querySelector("imgis");
+	recebe.appendChild(divs);
+
+
+
+}
